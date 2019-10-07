e93cb4a4fbe608512f2ff6727f50082d032ea552
tral909 <indorm1992@mail.ru>
2019-10-07 21:38:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      let totalHours = knowsProgramming ? 800 : 1300;
+      return Math.ceil(totalHours / config[focus]);
   };
   
